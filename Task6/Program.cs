da20c383f653d710633dac10bc4aa840c02f0897
Maxim Prav <mxmlknv@mail.ru>
2023-02-04 19:52:23 +0300

    Task6 done

---
+++ Task6/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int evenNumber = number%2;
+if(evenNumber == 0)
+{
+    Console.Write("Число " + number + " является четным");
+}
+else
+{
+    Console.Write("Число " + number + " является нечетным");
+}
